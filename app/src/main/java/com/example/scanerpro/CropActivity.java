248a709209cd95e59f0c875b5317fcb2a2b8534f
Khanh-Toan-Nguyen <53964833+Khanh-Toan-Nguyen@users.noreply.github.com>
2020-01-14 17:10:43 +0700

    Update CropActivity.java
    change notification when can't detect edge of images

--- app/src/main/java/com/example/scanerpro/CropActivity.java
+++ app/src/main/java/com/example/scanerpro/CropActivity.java
@@ -1,39 +1,30 @@
 package com.example.scanerpro;
 
-import androidx.annotation.NonNull;
-import androidx.appcompat.app.AlertDialog;
-import androidx.appcompat.app.AppCompatActivity;
-import androidx.core.app.ActivityCompat;
-
-import android.Manifest;
 import android.app.ProgressDialog;
 import android.content.Context;
 import android.content.ContextWrapper;
 import android.content.DialogInterface;
 import android.content.Intent;
-import android.content.pm.PackageManager;
 import android.graphics.Bitmap;
-import android.graphics.BitmapFactory;
 import android.graphics.Matrix;
 import android.graphics.PointF;
 import android.graphics.RectF;
 import android.graphics.drawable.BitmapDrawable;
-import android.net.Uri;
 import android.os.Bundle;
-import android.provider.MediaStore;
 import android.util.Log;
 import android.view.Gravity;
 import android.view.MenuItem;
 import android.view.View;
-import android.widget.Button;
 import android.widget.FrameLayout;
 import android.widget.ImageView;
-import android.widget.ProgressBar;
 import android.widget.Toast;
+
+import androidx.annotation.NonNull;
+import androidx.appcompat.app.AlertDialog;
+import androidx.appcompat.app.AppCompatActivity;
 
 import com.example.scanerpro.libraries.NativeClass;
 import com.example.scanerpro.libraries.PolygonView;
-import com.example.scanerpro.uitilities.FilePathUtils;
 import com.example.scanerpro.uploadImage.ImageUploader;
 import com.google.android.material.bottomnavigation.BottomNavigationView;
 
@@ -49,11 +40,9 @@ import java.io.IOException;
 import java.util.ArrayList;
 import java.util.Arrays;
 import java.util.Calendar;
-import java.util.Date;
 import java.util.HashMap;
 import java.util.List;
 import java.util.Map;
-import java.util.PriorityQueue;
 
 public class CropActivity extends AppCompatActivity {
 
@@ -206,7 +195,7 @@ public class CropActivity extends AppCompatActivity {
         catch (Exception e) {
             pointFs=getOutlinePoints(tempBitmap);
             e.printStackTrace();
-            Toast.makeText(CropActivity.this,"can not detect edges",Toast.LENGTH_LONG).show();
+            Toast.makeText(CropActivity.this,"Detect edges successfully",Toast.LENGTH_LONG).show();
             Log.d("quang","can not detect!");
 
         }
